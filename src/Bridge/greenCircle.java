f2ec1493a89e4f6eb235f4d76a929bff52cc4a48
Sahaban Arshad <Sahaban Arshad@192.168.10.35>
2016-06-13 23:21:09 +0500

    jenkins

--- src/Bridge/greenCircle.java
+++ src/Bridge/greenCircle.java
@@ -4,7 +4,8 @@ public class greenCircle implements drawAPI{
 
 	@Override
 	public void drawCircle(int rad,int x,int y) {
-		System.out.println("Now we dra a Green cirle with radious="+rad+" x "+x+" y "+y);		
+		System.out.println("Now we dra a Green cirle with radious="+rad+" x "+x+" y "+y);
+		System.out.println("Now we dra a Green cirle with radious="+rad+" x "+x+" y "+y);
 	}
 
 }
