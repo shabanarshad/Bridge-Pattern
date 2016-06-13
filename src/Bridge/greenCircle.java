43be97994e10cde8b52f9aa66d9c28a5139b40d4
Sahaban Arshad <Sahaban Arshad@192.168.10.35>
2016-06-13 23:38:01 +0500

    jenkins 2

--- src/Bridge/greenCircle.java
+++ src/Bridge/greenCircle.java
@@ -6,6 +6,7 @@ public class greenCircle implements drawAPI{
 	public void drawCircle(int rad,int x,int y) {
 		System.out.println("Now we dra a Green cirle with radious="+rad+" x "+x+" y "+y);
 		System.out.println("Now we dra a Green cirle with radious="+rad+" x "+x+" y "+y);
+		System.out.println("Now we dra a Green cirle with radious="+rad+" x "+x+" y "+y);
 	}
 
 }
