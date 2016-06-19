fafeb66d5ad13bd5e0049aa7a2f2db34c2307898
Sahaban Arshad <Sahaban Arshad@192.168.10.46>
2016-06-19 16:10:48 +0500

    okok

---
+++ src/Bridge/circleTest.java
@@ -0,0 +1,24 @@
+package Bridge;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+/**
+*@author Sahaban Arshad
+*@virsion 1.0 Jun 19, 2016
+*
+*/
+public class circleTest {
+
+	@Test
+	public void testDraw() {
+		JUnitCore circle =new JUnitCore();
+	}
+
+	@Test
+	public void testCircle() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/Bridge/demoTest.java
@@ -0,0 +1,19 @@
+package Bridge;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+*@author Sahaban Arshad
+*@virsion 1.0 Jun 19, 2016
+*
+*/
+public class demoTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
